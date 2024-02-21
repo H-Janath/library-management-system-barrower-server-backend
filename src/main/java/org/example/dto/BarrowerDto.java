b31e438bb2e91bbae4036cd946e378daeffaa5fa
janath <janathhma@gmail.com>
2024-02-21 19:44:14 +0530

    Define BarrowerDto class

---
+++ src/main/java/org/example/dto/BarrowerDto.java
@@ -0,0 +1,9 @@
+package org.example.dto;
+
+public class BarrowerDto {
+    private Long BID;
+    private String barrower_name;
+    private String contact_no;
+    private String address;
+    private String NIC;
+}
