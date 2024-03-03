f073cf0e5c421712adec6ee920249eb1547340fb
janath <janathhma@gmail.com>
2024-03-03 20:07:43 +0530

    define user dto class

---
+++ src/main/java/org/example/dto/UserDto.java
@@ -0,0 +1,13 @@
+package org.example.dto;
+
+public class UserDto {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String address1;
+    private String address2;
+    private String country;
+    private String number;
+
+}
