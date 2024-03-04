4d06c89a34ca9360fb93211344fe2c25bab3f99c
janath <janathhma@gmail.com>
2024-03-04 06:48:27 +0530

    Design new API for add new barrower

--- src/main/java/org/example/controller/Barrowercontroller.java
+++ src/main/java/org/example/controller/Barrowercontroller.java
@@ -1,7 +1,9 @@
 package org.example.controller;
 import lombok.RequiredArgsConstructor;
+import org.example.Entity.CustomResponse;
 import org.example.dto.BarrowerDto;
 import org.example.service.BarrowerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +23,7 @@ public class Barrowercontroller {
     }
 
     @DeleteMapping("/delete/{bid}")
-    public ResponseEntity<String> deleteBarrower(@PathVariable Long bid){
+    public ResponseEntity<String> deleteBarrower(@PathVariable String bid){
         Optional<BarrowerDto> optionalBarrowerDto = barrowerService.findByID(bid);
         if(optionalBarrowerDto.isPresent()){
             barrowerService.deleteBarrower(bid);
@@ -30,14 +32,28 @@ public class Barrowercontroller {
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<String> addBarrowers(@RequestBody BarrowerDto barrowerDto){
+    @PostMapping("/update")
+    public ResponseEntity<String> updateBarrowers(@RequestBody BarrowerDto barrowerDto){
         Optional<BarrowerDto> optionalBarrowerDto = barrowerService.findByID(barrowerDto.getBid());
         if(optionalBarrowerDto.isPresent()){
-            barrowerService.addBarrower(barrowerDto);
+            barrowerService.updateBarrower(barrowerDto);
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<CustomResponse<String>> addBorrower(@RequestBody BarrowerDto barrowerDto) {
+        BarrowerDto addedBorrower = barrowerService.addBorrower(barrowerDto);
+
+        if (addedBorrower != null && addedBorrower.getBid() !=null) {
+            CustomResponse<String> customResponse = new CustomResponse<>(addedBorrower.getBid(), "Success");
+            return new ResponseEntity<>(customResponse, HttpStatus.OK);
+        } else {
+            CustomResponse<String> customResponse = new CustomResponse<>(addedBorrower.getBid(),"Unsuccessful");
+            return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 }
