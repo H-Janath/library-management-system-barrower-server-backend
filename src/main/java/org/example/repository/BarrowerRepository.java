2adda55c926583069780e8ae623b99926065657e
janath <janathhma@gmail.com>
2024-03-07 09:47:42 +0530

    Add query to find max ID in Barrower table

--- src/main/java/org/example/repository/BarrowerRepository.java
+++ src/main/java/org/example/repository/BarrowerRepository.java
@@ -2,6 +2,7 @@ package org.example.repository;
 
 import org.example.Entity.Barrower;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface BarrowerRepository extends JpaRepository<Barrower,Long> {
         Barrower findBybid(String bid);
         Barrower deleteBybid(String bid);
+
+        @Query("SELECT MAX(b.id) FROM Barrower b")
+        Long findMaxID();
 }
