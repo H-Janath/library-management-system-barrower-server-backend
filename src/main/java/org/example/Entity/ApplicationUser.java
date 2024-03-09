2330645bc6a8d30d639375abf240b115cdde0ce7
janath <janathhma@gmail.com>
2024-03-09 07:26:16 +0530

    create UserService class

--- src/main/java/org/example/Entity/ApplicationUser.java
+++ src/main/java/org/example/Entity/ApplicationUser.java
@@ -18,96 +18,96 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="users")
 public class ApplicationUser implements UserDetails{
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer userId;
-    @Column(unique=true)
-    private String username;
-    private String password;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer userId;
+	@Column(unique=true)
+	private String username;
+	private String password;
 
-    @ManyToMany(fetch=FetchType.EAGER)
-    @JoinTable(
-            name="user_role_junction",
-            joinColumns = {@JoinColumn(name="user_id")},
-            inverseJoinColumns = {@JoinColumn(name="role_id")}
-    )
-    private Set<Role> authorities;
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(
+			name="user_role_junction",
+			joinColumns = {@JoinColumn(name="user_id")},
+			inverseJoinColumns = {@JoinColumn(name="role_id")}
+	)
+	private Set<Role> authorities;
 
-    public ApplicationUser() {
-        super();
-        authorities = new HashSet<>();
-    }
+	public ApplicationUser() {
+		super();
+		authorities = new HashSet<>();
+	}
 
 
-    public ApplicationUser(Integer userId, String username, String password, Set<Role> authorities) {
-        super();
-        this.userId = userId;
-        this.username = username;
-        this.password = password;
-        this.authorities = authorities;
-    }
+	public ApplicationUser(Integer userId, String username, String password, Set<Role> authorities) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.authorities = authorities;
+	}
 
-    public Integer getUserId() {
-        return this.userId;
-    }
+	public Integer getUserId() {
+		return this.userId;
+	}
 
-    public void setId(Integer userId) {
-        this.userId = userId;
-    }
+	public void setId(Integer userId) {
+		this.userId = userId;
+	}
 
-    public void setAuthorities(Set<Role> authorities) {
-        this.authorities = authorities;
-    }
+	public void setAuthorities(Set<Role> authorities) {
+		this.authorities = authorities;
+	}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        return this.authorities;
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return this.authorities;
+	}
 
-    @Override
-    public String getPassword() {
-        // TODO Auto-generated method stub
-        return this.password;
-    }
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    @Override
-    public String getUsername() {
-        // TODO Auto-generated method stub
-        return this.username;
-    }
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    /* If you want account locking capabilities create variables and ways to set them for the methods below */
-    @Override
-    public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        return true;
-    }
+	/* If you want account locking capabilities create variables and ways to set them for the methods below */
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        return true;
-    }
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        return true;
-    }
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        return true;
-    }
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
 }
